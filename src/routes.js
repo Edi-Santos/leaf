1190daa4217f9ddec6c4d3b820862f1d332ef135
Edi-Santos <edsantos2822@gmail.com>
2024-05-27 19:03:48 -0300

    chore:(routes.js): corringdo erros de Linter e adicionando rota para login

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Route, Routes as RoutesDom } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function Routes() {
   return (
     <RoutesDom>
-      <Route index element={ <Home /> } />
+      <Route index element={<Home />} />
+      <Route path='/login' element={<Login />} />
     </RoutesDom>
   )
 }
