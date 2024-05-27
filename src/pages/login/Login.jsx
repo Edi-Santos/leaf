50217487e707e99efa474665d4679a1e46966f6a
Edi-Santos <edsantos2822@gmail.com>
2024-05-27 19:05:58 -0300

    feat:(Login.jsx): implementando página e navegação para página Home

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div>
+      <h1>LOGIN</h1>
+
+      <Link to={'/'}>HOME</Link>
+    </div>
+  );
+}
+
+export default Login;
