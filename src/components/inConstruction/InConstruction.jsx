bf6d088fda0afd232029d147785641c6a1941dbb
Edi-Santos <edsantos2822@gmail.com>
2024-05-30 16:16:38 -0300

    chore:(inConstruction/): centralizando consteúdo e modificando tamanho do gif

--- src/components/inConstruction/InConstruction.jsx
+++ src/components/inConstruction/InConstruction.jsx
@@ -3,8 +3,8 @@ import './inConstruction.css';
 
 function InConstruction() {
   return (
-    <div>
-      <h1 className='in-construction-text'>Em Construção...</h1>
+    <div className='in-construction-container'>
+      <h1 className='in-construction-text'>Página em Construção...</h1>
       <iframe
         className='in-construction-img'
         src='https://giphy.com/embed/jtXxlGob7X9wJlOfCc'
