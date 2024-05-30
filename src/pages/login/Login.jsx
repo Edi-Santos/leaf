4a2a683854fef685545173e0ed35d2d34b30d8ec
Edi-Santos <edsantos2822@gmail.com>
2024-05-30 16:20:07 -0300

    feature:(Login.jsx): implementando página de login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,11 +3,34 @@ import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <form>
+      <h2>LOGIN</h2>
 
-      <Link to={'/'}>HOME</Link>
-    </div>
+      <label htmlFor='login-e-mail'>
+        E-mail
+        <input
+          type='email'
+          id='login-e-mail'
+          value=''
+          placeholder='exemplo@email.com'
+        />
+      </label>
+
+      <label htmlFor='login-password'>
+        Senha
+        <input
+          type='password'
+          id='login-password'
+          value=''
+          placeholder='********'
+        />
+      </label>
+
+      <div>
+        <button type='button'>ENTRAR</button>
+        <Link to={'/'}>VOLTAR</Link>
+      </div>
+    </form>
   );
 }
 
